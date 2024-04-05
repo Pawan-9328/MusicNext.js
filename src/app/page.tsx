f1367ec42c3bce7301aa643ec721c0a5f298306a
Pawan Gola <pawangola908@gmail.com>
2024-04-06 04:59:33 +0530

    Solving Hero section Challenges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-  <h1 className="text-2xl text-center" >Keshav Code</h1>
+  
+      <HeroSection />
     </main>
-     
   );
 }
