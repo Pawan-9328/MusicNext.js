cd389d03d0c230af41c40b6732a68c7ec7189456
Pawan Gola <pawangola908@gmail.com>
2024-04-06 07:19:41 +0530

    Animated tooltip and wavy background

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// here we mentioned image docs path: to reduce the next.js error
+const nextConfig = {
+     images: {
+        domains: ['images.unsplash.com', 'res.cloudinary.com']
+     }
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/WhyChooseUs";
 import MusicSchoolTextimonials from "./components/TextimonialCards"
 import  UpcomingWebinars from "./components/UpcomingWebinars"
+import Instructors from "./components/Instructors";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <WhyChooseUs/>
       <MusicSchoolTextimonials/>
       <UpcomingWebinars/>
+      <Instructors/>
     </main>
   );
 }
