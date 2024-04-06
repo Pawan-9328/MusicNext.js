cc9a2a9edf3d8f508609f832a427d4bacb37ba66
Pawan Gola <pawangola908@gmail.com>
2024-04-06 05:47:51 +0530

    Building Featured Card section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -28,6 +28,7 @@ function HeroSection() {
                 <Button 
                  borderRadius="1.75rem"
                  className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                 
                 >
                     Explore Courses
                 </Button>
